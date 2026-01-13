3662fb1ce803828a6e2c7e8a0c516d36acc38d63
Hugo Araya Nash <kabroxiko@gmail.com>
2026-01-13 20:59:34 -0300

    reorder debug.h

--- src/debug.h
+++ src/debug.h
@@ -1,3 +1,4 @@
+
 #pragma once
 #include <Arduino.h>
 
@@ -5,58 +6,32 @@
 inline void debugPrintln() { Serial.println(); }
 inline void debugPrintln(const char* msg) { Serial.println(msg); }
 inline void debugPrintln(const String& msg) { Serial.println(msg); }
-inline void debugPrintln(String& msg) { Serial.println(msg); }
 inline void debugPrintln(int val) { Serial.println(val); }
+inline void debugPrintln(unsigned int val) { Serial.println(val); }
 inline void debugPrintln(unsigned long val) { Serial.println(val); }
-template<typename T>
-inline void debugPrintln(const T& val) {
-#ifdef DEBUG_SERIAL
-	Serial.println(val);
-#else
-	(void)val;
-#endif
-}
+inline void debugPrintln(unsigned long val, int base) { Serial.println(val, base); }
 
-template<typename T>
-inline void debugPrint(const T& val) {
-#ifdef DEBUG_SERIAL
-	Serial.print(val);
-#else
-	(void)val;
-#endif
-}
 inline void debugPrint(const char* msg) { Serial.print(msg); }
 inline void debugPrint(const String& msg) { Serial.print(msg); }
-inline void debugPrint(String& msg) { Serial.print(msg); }
 inline void debugPrint(int val) { Serial.print(val); }
+inline void debugPrint(unsigned int val) { Serial.print(val); }
 inline void debugPrint(unsigned long val) { Serial.print(val); }
+inline void debugPrint(unsigned long val, int base) { Serial.print(val, base); }
 #else
 inline void debugPrintln() {}
 inline void debugPrintln(const char*) {}
 inline void debugPrintln(const String&) {}
-inline void debugPrintln(String&) {}
 inline void debugPrintln(int) {}
+inline void debugPrintln(unsigned int) {}
 inline void debugPrintln(unsigned long) {}
-template<typename T>
-inline void debugPrintln(const T& val) {
-#ifdef DEBUG_SERIAL
-	Serial.println(val);
-#else
-	(void)val;
-#endif
-}
+inline void debugPrintln(uint32_t) {}
+inline void debugPrintln(uint32_t, int) {}
 
-template<typename T>
-inline void debugPrint(const T& val) {
-#ifdef DEBUG_SERIAL
-	Serial.print(val);
-#else
-	(void)val;
-#endif
-}
 inline void debugPrint(const char*) {}
 inline void debugPrint(const String&) {}
-inline void debugPrint(String&) {}
 inline void debugPrint(int) {}
+inline void debugPrint(unsigned int) {}
 inline void debugPrint(unsigned long) {}
+inline void debugPrint(uint32_t) {}
+inline void debugPrint(uint32_t, int) {}
 #endif
