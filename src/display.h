855d533bb814630f1e1e4ac43d69fe1bada4072b
Hugo Araya Nash <kabroxiko@gmail.com>
2026-01-15 13:48:25 -0300

    add display

--- src/display.h
+++ src/display.h
@@ -1,12 +1,12 @@
 #pragma once
 #define TFT_WIDTH  80
 #define TFT_HEIGHT 160
-#define TFT_SCL    14   // SCL (SCK/CLK)
-#define TFT_SDA    12   // SDA (MOSI/DIN)
+#define TFT_SCL    12   // SCL (SCK/CLK)
+#define TFT_SDA    14   // SDA (MOSI/DIN)
 #define TFT_RES    27   // Reset
 #define TFT_DC     26   // Data/Command
-#define TFT_CS     25   // Chip Select
-#define TFT_BLK    33   // Backlight
+#define TFT_CS     33   // Chip Select
+#define TFT_BLK    25   // Backlight
 #define TFT_DRIVER ST7735S
 
 void setup_display();
