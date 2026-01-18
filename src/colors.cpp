d79d1b070e1d807fa80c01bbfa243ee65d0b4f9b
Hugo Araya Nash <kabroxiko@gmail.com>
2026-01-18 10:21:43 -0300

    migrate to neopixel

--- src/colors.cpp
+++
@@ -1,4 +0,0 @@
-#include "colors.h"
-
-// All functions are inline in the header for performance and header-only usage.
-// If you want to move implementation here, remove 'inline' from header and define here.
